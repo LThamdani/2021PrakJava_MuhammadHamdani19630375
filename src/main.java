41dfff976ac92afa7ce3c304f0504a0d520338e1
LThamdani <lt.hamdani.x@gmail.com>
2021-06-08 10:42:31 -0700

    membuat main class

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        String namaDepan = "Letnan";
+        String namaTengah = "Muhammad";
+        String namaBelakang = "Hamdani";
+
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.8889;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan+namaTengah+namaBelakang);
+    }
+}
