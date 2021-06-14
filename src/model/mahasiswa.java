18d5b3fb627bf8841d9dfc2692242118874840c4
LThamdani <lt.hamdani.x@gmail.com>
2021-06-13 22:34:48 -0700

    hitung usia

--- src/model/mahasiswa.java
+++ src/model/mahasiswa.java
@@ -2,6 +2,7 @@ package model;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class mahasiswa {
@@ -16,6 +17,34 @@ public class mahasiswa {
             System.out.println("NPM \t\t\t : " + this.npm);
             System.out.println("Nama \t\t\t : " + this.nama);
             System.out.println("Tanggal Lahir \t : " + sdf.format(this.tangallahir));
+        System.out.println("usia \t\t\t : " + hitungUsia());
 
+    }
+    String getNama(){
+        return nama;
+    }
+    int hitungUsia() {
+        Calendar tanggallahir = Calendar.getInstance();
+        tanggallahir.setTime(this.tangallahir);
+        Calendar tanggalIni = Calendar.getInstance();
+        int tahunLahir = tanggallahir.get(Calendar.YEAR);
+        int tahunIni = tanggalIni.get(Calendar.YEAR);
+        int selisihTahun = tahunIni - tahunLahir;
+        int bulanLahir = tanggallahir.get(Calendar.MONTH);
+        int bulanIni = tanggalIni.get(Calendar.MONTH);
+
+        if(bulanIni<bulanLahir){
+            selisihTahun--;
+        }else{
+            int tanggalTanggallahir = tanggallahir.get(Calendar.DAY_OF_MONTH);
+            int tanggalTanggalIni = tanggalIni.get(Calendar.DAY_OF_MONTH);
+            if(bulanIni == bulanLahir){
+                if(tanggalTanggalIni < tanggalTanggallahir){
+                    selisihTahun--;
+                }
+            }
+        }
+
+        return selisihTahun;
     }
 }
